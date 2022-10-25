d0ba76702f64707ec81f6cadc676d1db5889e55b
Galib6 <asadullahalgalib81@gmail.com>
2022-10-25 16:31:32 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require('./data/categories.json');
+const coursesDetails = require('./data/coursesDetails.json');
+
+app.get('/', (req, res) => {
+    res.send('API Running');
+});
+
+app.get('/category', (req, res) => {
+    res.send(categories)
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    const category_course = coursesDetails.filter(c => c.category_id === id);
+    res.send(category_course);
+
+})
+
+app.get('/courses', (req, res) => {
+    res.send(coursesDetails);
+});
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourses = coursesDetails.find(c => c.category_id === id);
+    res.send(selectedCourses);
+});
+
+app.listen(port, () => {
+    console.log('Courses Api Running', port);
+})
