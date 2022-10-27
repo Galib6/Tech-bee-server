be5c12872857d31218509faf1b686562173cca51
Galib6 <asadullahalgalib81@gmail.com>
2022-10-27 22:43:09 +0600

    function problem fixed

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.get('/', (req, res) => {
 app.get('/category', (req, res) => {
     res.send(categories)
 });
+
 app.get('/category/:id', (req, res) => {
     const id = req.params.id;
     if (id === "01") {
